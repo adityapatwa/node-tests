abeac74a3b62df2c1db1cd10e6a6da89bec80d11
Aditya Patwa <adityapatwa@live.com>
2018-09-24 16:40:17 -0400

    Organized the test cases using the describe function

--- server/server.test.js
+++ server/server.test.js
@@ -3,23 +3,29 @@ const expect = require('expect');
 
 let app = require('./server').app;
 
-it('should return 404', (done) => {
-   request(app)
-       .get('/')
-       .expect((res) => {
-           expect(res.body).toInclude({
-               error: 'Page not found'
-           })
-       })
-       .end(done);
-});
+describe('Server', () => {
+    describe('GET /', () => {
+        it('should return 404', (done) => {
+            request(app)
+                .get('/')
+                .expect((res) => {
+                    expect(res.body).toInclude({
+                        error: 'Page not found'
+                    })
+                })
+                .end(done);
+        });
+    });
 
-it('should expect my user object', (done) => {
-    request(app)
-        .get('/users')
-        .expect(200)
-        .expect((res) => {
-            expect(res.body).toInclude({name: 'Aditya', age: 26})
-        })
-        .end(done);
+    describe('GET /users', () => {
+        it('should expect my user object', (done) => {
+            request(app)
+                .get('/users')
+                .expect(200)
+                .expect((res) => {
+                    expect(res.body).toInclude({name: 'Aditya', age: 26})
+                })
+                .end(done);
+        });
+    });
 });
--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,47 +1,54 @@
 const expect = require('expect');
 const utils = require('./utils');
 
-it('should add two numbers', () => {
-    let res = utils.add(33, 11);
+describe('Utils', () => {
+    describe('#add', () => {
+        it('should add two numbers', () => {
+            let res = utils.add(33, 11);
 
-    expect(res).toBe(44).toBeA('number');
-    // if (res !== 44) {
-    //     throw new Error(`Expected 44, but got ${res}`);
-    // }
-});
+            expect(res).toBe(44).toBeA('number');
+            // if (res !== 44) {
+            //     throw new Error(`Expected 44, but got ${res}`);
+            // }
+        });
 
-it('should async add two numbers', (done) => {
-    utils.asyncAdd(3, 5, (sum) => {
-        expect(sum).toBe(8).toBeA('number');
-        done();
+        it('should async add two numbers', (done) => {
+            utils.asyncAdd(3, 5, (sum) => {
+                expect(sum).toBe(8).toBeA('number');
+                done();
+            });
+        });
+    });
+
+    describe('#square', () => {
+        it('should square a number', () => {
+            let res = utils.square(11);
+
+            expect(res).toBe(121).toBeA('number');
+            // if (res !== 121) {
+            //     throw new Error(`Expected 121, but got ${res}`);
+            // }
+        });
+
+        it('should async square a number', (done) => {
+            utils.asyncSquare(4, (square) => {
+                expect(square).toBe(16).toBeA('number');
+                done();
+            });
+
+        });
+    });
+
+    it ('should verify first and last names are set', () => {
+        let res = utils.setName({age: 2, location: 'Philadelphia'},'Aditya Patwa');
+
+        expect(res).toInclude({
+            firstName: 'Aditya',
+            lastName: 'Patwa'
+        });
     });
 });
 
-it('should square a number', () => {
-    let res = utils.square(11);
-
-    expect(res).toBe(121).toBeA('number');
-    // if (res !== 121) {
-    //     throw new Error(`Expected 121, but got ${res}`);
-    // }
-});
-
-it('should async square a number', (done) => {
-    utils.asyncSquare(4, (square) => {
-        expect(square).toBe(16).toBeA('number');
-        done();
-    });
-
-});
-
-it ('should verify first and last names are set', () => {
-    let res = utils.setName({age: 2, location: 'Philadelphia'},'Aditya Patwa');
-
-    expect(res).toInclude({
-        firstName: 'Aditya',
-        lastName: 'Patwa'
-    });
-});
 
 // it('should expect some values', () => {
 //     // expect(12).toNotBe(12);
