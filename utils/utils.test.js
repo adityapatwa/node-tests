72b94f371fccab72d027778ab1a8985101af6a63
Aditya Patwa <adityapatwa@live.com>
2018-09-24 15:28:15 -0400

    Used an assertion library to perform testing in NodeJS

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,18 +1,42 @@
+const expect = require('expect');
 const utils = require('./utils');
+
 it('should add two numbers', () => {
     let res = utils.add(33, 11);
 
-    if (res !== 44) {
-
-        throw new Error(`Expected 44, but got ${res}`);
-    }
+    expect(res).toBe(44).toBeA('number');
+    // if (res !== 44) {
+    //     throw new Error(`Expected 44, but got ${res}`);
+    // }
 });
 
 it('should square a number', () => {
     let res = utils.square(11);
 
-    if (res !== 121) {
-
-        throw new Error(`Expected 121, but got ${res}`);
-    }
+    expect(res).toBe(121).toBeA('number');
+    // if (res !== 121) {
+    //     throw new Error(`Expected 121, but got ${res}`);
+    // }
 });
+
+it ('should verify first and last names are set', () => {
+    let res = utils.setName({age: 2, location: 'Philadelphia'},'Aditya Patwa');
+
+    expect(res).toInclude({
+        firstName: 'Aditya',
+        lastName: 'Patwa'
+    });
+});
+
+// it('should expect some values', () => {
+//     // expect(12).toNotBe(12);
+//     // expect*{name: 'Andrew'}.toNotEqual({name: 'Andrew'});
+//     // expect([2,3,4]).toExclude(1);
+//     expect({
+//         name: 'Andrew',
+//         age: 25,
+//         location: 'Philadelphia'
+//     }).toExclude({
+//         age: 23
+//     });
+// });
